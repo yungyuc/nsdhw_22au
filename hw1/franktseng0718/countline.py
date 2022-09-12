542c50f806272b96189628d5c371b13ef6b21a62
franktseng0718 <franktseng0718@gmail.com>
2022-09-12 22:23:14 +0800

    franktseng0718-hw1-attempt1

---
+++ hw1/franktseng0718/countline.py
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+code = "
+import sys
+import os.path
+
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
+"
+
+if which $PYTHON_BIN > /dev/null; then
+   $PYTHON_BIN -c "$code" "$@"
+else
+    echo "exec: $PYTHON_BIN: not found"
+    exit 127
+fi
+
