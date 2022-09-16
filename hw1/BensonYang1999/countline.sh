bef705d5fa0fe1ff36fb27259e48c5bf8b76b4ca
Benson Yang <benson2219@gmail.com>
2022-09-17 03:31:14 +0800

    add detection of "incorrect number of parameters" & solve "$'\r':command not found" problem

--- hw1/BensonYang1999/countline.sh
+++ hw1/BensonYang1999/countline.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-filename=$1
-if [ -f $filename ]
-then
-    n=0
-    while read line; do
-        n=$((n+1))
-    done < $filename
-
-    echo "$n lines in $filename"
+if [ "$#" -lt 1 ]; then
+    echo "missing file name"
+elif [ "$#" -gt 1 ]; then
+    echo "only one argument is allowed"
 else
-    echo "$filename not found"
+    filename=$1
+    if [ -f $filename ]; then
+        n=0
+        while read line; do
+            n=$((n + 1))
+        done <$filename
+
+        echo "$n lines in $filename"
+    else
+        echo "$filename not found"
+    fi
 fi
