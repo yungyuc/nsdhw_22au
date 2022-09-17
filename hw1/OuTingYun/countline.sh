3e874ece0cc09d2caa725824377a1cc2d4a95315
OuTingYun <outingyun7897@gmail.com>
2022-09-17 20:49:55 +0800

    first

---
+++ hw1/OuTingYun/countline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ $# -lt 1 ]
+then
+    echo "missing file name"
+elif [ $# -gt 1 ]
+then
+    echo "only one argument is allowed"
+else
+    if [ -f $1 ]
+    then
+        result=$(wc -l $1 | tr -dc '0-9')
+        echo "$result lines in $1"
+    else
+        echo "$1 not found"
+    fi
+fi
