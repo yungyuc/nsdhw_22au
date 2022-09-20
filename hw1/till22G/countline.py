1f847b8782a45a88c7c456cdda9e70a262ce0977
till22G <tillman.galla@online.de>
2022-09-20 18:00:57 +0800

    code cleanup; hw1 attempt 1

--- hw1/till22G/countline.py
+++ hw1/till22G/countline.py
@@ -1,14 +1,9 @@
 #! /bin/bash
 
 bin=$PYTHON_BIN
-#bin=python3
 
 if [ -z "$(command -v $bin)" ]; then   
     echo  "exec: $bin: not found"
 fi
-
-#if [ "$PYTHON_BIN" == "python0" ]; then
-#    echo "exec: python0: not found"
-#fi
 
 wc -l "$1"
