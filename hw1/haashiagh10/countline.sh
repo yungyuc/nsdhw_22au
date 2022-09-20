521886b07f30e5c675017285360645d8542f71c0
weinien <haashiagh10.eed06@nctu.edu.tw>
2022-09-20 18:07:18 +0800

    test

--- hw1/haashiagh10/countline.sh
+++ hw1/haashiagh10/countline.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-#
-#Test for countline.py
-#
 
-if [ $# -lt 1 ]
-then
-	echo missing the file
-elif [ $# -gt 1 ]
-then
-	echo only one argument is allow
-elif [ -f $1 ]
-then
-	echo "$(wc -l $1 | tr -dc '0-9') lines in $1"
+if [ $# -eq 0 ] ; then
+    echo "missing file name"
+elif [ $# -eq 1 ] ; then
+    if [ -f $1 ] ; then
+        numLine="$(wc -l $1 | tr -dc '0-9')"
+        echo "$numLine"
+    else
+        echo "$1 not found"
+    fi
 else
-	echo "$1 not found"
-	
+    echo "only one argument is allowed"
 fi
-
