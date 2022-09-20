c846034a08f7af3758a8f65ffc4e27b001691d81
weinien <haashiagh10.eed06@nctu.edu.tw>
2022-09-20 17:01:06 +0800

    hw1
    test for hw1

---
+++ hw1/haashiagh10/countline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#Test for countline.py
+#
+
+if [ $# -lt 2 ]
+then
+	echo missing the file
+elif [ $# -gy 2 ]
+then
+	echo only one argument is allow
+else 
+	filename ="$1"
+	if [ -f $1 ];then
+		count = $(wc -l $1)
+		echo "$count lines in $1"
+	else
+		echo "file not found"
+	fi
+fi
+
