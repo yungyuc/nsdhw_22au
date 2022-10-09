c861fef82a6b2e51e20217cdef16d5042fa7d7df
zurzeit <nob237k@gmail.com>
2022-10-09 12:51:30 +0800

    q2 done

--- hw2/zurzeit/q2/q2.cpp
+++ hw2/zurzeit/q2/q2.cpp
@@ -1,16 +1,33 @@
 #include <pybind11/pybind11.h>
+#include <math.h>
 
 namespace py = pybind11;
 
-int func1(int a, int b){
-    if(a >= b)
-        return a;
-    return b;
-}
+class Vec{
+private:
+    float X1, Y1, X2, Y2;
+public:
+    Vec(float x1, float y1, float x2, float y2){
+        X1 = x1;
+        Y1 = y1;
+        X2 = x2;
+        Y2 = y2;
+    }
+    float angle_in_radian(){
+        float dot = X1*X2 + Y1*Y2;
+        float det = X1*Y2 - Y1*X2;
+        float angle = atan2(det, dot);
+        return angle;
+    }
+};
 
 
-
-PYBIND11_MODULE(_vector, handle){
+PYBIND11_MODULE(_vector, handle) {
     handle.doc() = "This is doc.";
-    handle.def("angle_between", &func1);
+    py::class_<Vec>(
+        handle, "pyVec"
+    )
+    .def(py::init<float,float,float,float>())
+    .def("angle", &Vec::angle_in_radian)
+    ;
 }
--- hw2/zurzeit/q2/test_q2.py
+++ hw2/zurzeit/q2/test_q2.py
@@ -1,9 +1,13 @@
 import _vector
+import math
 
 def test_zero_angle():
-	assert 1==1
+	vec1 = _vector.pyVec(1,0,2,0)
+	assert round(vec1.angle(),6) == round(math.atan2(0,1), 6)
 	return
-def test_zero_length():
-	assert 2==2
+def test_right_angle():
+	vec2 = _vector.pyVec(1,0,0,1)
+	print(f'c++:{vec2.angle()}')
+	print(f'python:{math.atan2(1,0)}')
+	assert round(vec2.angle(),6) == round(math.atan2(1,0),6)
 	return
-	
