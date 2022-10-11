f60701ec4510167e69860cc118e5d4e573df8780
liuallen871219 <liuallen871219@gmail.com>
2022-10-11 13:18:18 +0800

    first attempt

---
+++ hw2/liuallen871219/q1/line.hpp
@@ -0,0 +1,23 @@
+#include<vector>
+using namespace std;
+class Line
+{
+public:
+    Line();
+    Line(Line const & );
+    Line(Line       &&);
+    Line & operator=(Line const & );
+    Line & operator=(Line       &&);
+    Line(size_t size);
+    ~Line();
+    size_t size() const;
+    float const & x(size_t it) const;
+    float & x(size_t it);
+    float const & y(size_t it) const;
+    float & y(size_t it);
+private:
+    // Member data.
+    vector<float> x_p;
+    vector<float> y_p;
+    size_t len;
+}; /* end class Line */
