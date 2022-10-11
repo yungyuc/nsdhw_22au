ffab54540d0a8a25f676cf23b69ba791556552fb
weinien <haashiagh10.eed06@nctu.edu.tw>
2022-10-11 20:14:57 +0800

    Update line.cpp

--- hw2/haashiagh10/q1/line.cpp
+++ hw2/haashiagh10/q1/line.cpp
@@ -1,13 +1,48 @@
-#include "line.hpp"
-#include <cstdint>
-#include <stdexcept>
-#include <vector>
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int main(int, char **)
-{
+class Line {
+public:
+    Line() = default;
+    Line(Line const&) = default;
+    Line(Line&&) = default;
+    Line& operator=(Line const&) = default;
+    Line& operator=(Line&&) = default;
+    ~Line() = default;
+
+    Line(size_t size) {
+        _x.resize(size);
+        _y.resize(size);
+    }
+
+    size_t size() const { 
+        return _x.size(); 
+    }
+
+    float& x(size_t it) {
+        return _x[it];
+    }
+
+    float const& x(size_t it) const {
+        return _x[it];
+    }
+
+    float& y(size_t it) {
+        return _y[it];
+    }
+
+    float const& y(size_t it) const {
+        return _y[it];
+    }
+
+private:
+    vector<float> _x, _y;
+
+    }
+}; 
+
+int main(int, char **) {
     Line line(3);
     line.x(0) = 0; line.y(0) = 1;
     line.x(1) = 1; line.y(1) = 3;
@@ -17,16 +52,14 @@ int main(int, char **)
     line2.x(0) = 9;
 
     std::cout << "line: number of points = " << line.size() << std::endl;
-    for (size_t it=0; it<line.size(); ++it)
-    {
+    for (size_t it = 0; it < line.size(); it++) {
         std::cout << "point " << it << ":"
                   << " x = " << line.x(it)
                   << " y = " << line.y(it) << std::endl;
     }
 
     std::cout << "line2: number of points = " << line.size() << std::endl;
-    for (size_t it=0; it<line.size(); ++it)
-    {
+    for (size_t it = 0; it < line.size(); it++) {
         std::cout << "point " << it << ":"
                   << " x = " << line2.x(it)
                   << " y = " << line2.y(it) << std::endl;
