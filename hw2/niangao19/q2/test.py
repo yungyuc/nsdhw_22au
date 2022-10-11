f2e4350cffb96b88e971d747104567b3fbff75a9
niangao19 <55120492+niangao19@users.noreply.github.com>
2022-10-11 12:02:15 +0800

    Update test.py

--- hw2/niangao19/q2/test.py
+++ hw2/niangao19/q2/test.py
@@ -13,10 +13,10 @@ def test0():
 def test45():
     assert (calculates_angle([4.0, 0.0], [1.0, 1.0])) == approx(math.pi/4)
 # Test for zero angle
-def test3():
+def test90():
     assert (calculates_angle([1.0, 0.0], [0.0, 1.0])) == approx(math.pi/2)
 
 # Test for one other angle
-def test4():
+def test180():
     assert (calculates_angle([-1.0, 0.0], [1.0, 0.0])) == approx(math.pi)
 
