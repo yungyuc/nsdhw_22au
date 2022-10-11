5d58e943fc70a1657d2e4cf4a9eeeec513d0ec41
niangao19 <55120492+niangao19@users.noreply.github.com>
2022-10-11 12:01:40 +0800

    Update test.py

--- hw2/niangao19/q2/test.py
+++ hw2/niangao19/q2/test.py
@@ -1,19 +1,22 @@
-from _vector import ca
+from _vector import calculates_angle
 import math
 import pytest
 from pytest import approx
- 
-# Test for zero-length 2-vector
+
+# Test for zero-length 2-vector (invalid input)
+def testiv():
+    assert (calculates_angle([0.0, 0.0], [4.0, 0.0])) == 0.0
+# Test for zero angle
 def test0():
-    assert (calculateAngle([2.0, 0.0], [4.0, 0.0])) == 0.0
+    assert (calculates_angle([2.0, 0.0], [4.0, 0.0])) == 0.0
 #Test for right angle (90-deg)
 def test45():
-    assert (calculateAngle([4.0, 0.0], [1.0, 1.0])) == approx(math.pi/4)
+    assert (calculates_angle([4.0, 0.0], [1.0, 1.0])) == approx(math.pi/4)
 # Test for zero angle
 def test3():
-    assert (calculateAngle([1.0, 0.0], [0.0, 1.0])) == approx(math.pi/2)
+    assert (calculates_angle([1.0, 0.0], [0.0, 1.0])) == approx(math.pi/2)
 
 # Test for one other angle
 def test4():
-    assert (calculateAngle([-1.0, 0.0], [1.0, 0.0])) == approx(math.pi)
+    assert (calculates_angle([-1.0, 0.0], [1.0, 0.0])) == approx(math.pi)
 
