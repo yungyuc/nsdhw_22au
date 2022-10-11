e5e3939e4bea3b4d3250ebfc7ed9fde2f182f044
weinien <haashiagh10.eed06@nctu.edu.tw>
2022-10-11 20:50:14 +0800

    Update test_.py

--- hw2/haashiagh10/q2/test_.py
+++ hw2/haashiagh10/q2/test_.py
@@ -6,10 +6,10 @@ from _vector import pyb11_cal_angle
 
 def test_zero_len():
 	with pytest.raises(RuntimeError):
-		angle([0,0], [0,0])
+		pyb11_cal_angle([0,0], [0,0])
 
 def test_zero_ang():
-	assert angle([1,1], [1,1]) == pytest.approx(0)
+	assert pyb11_cal_angle([1,1], [1,1]) == pytest.approx(0)
 
 def test_right_ang():
-	assert angle([0,1], [1,0]) == pytest.approx(math.pi / 2)
+	assert pyb11_cal_angle([0,1], [1,0]) == pytest.approx(math.pi / 2)
