2ab1fe26a1e41a995cade4c960cde5754a0c1693
i-padiolleau <i-padiolleau@LAPTOP-B3M7QR7C.localdomain>
2022-10-12 00:14:17 +0800

    q2-4

--- hw2/i-padiolleau/q2/test_q2.py
+++ hw2/i-padiolleau/q2/test_q2.py
@@ -3,9 +3,9 @@ from _vector import comp_angle
 import math
 
 def test_0():
-    assert calc_angle(0, 0, 0, 0) == pytest.approx(-1)
+    assert comp_angle(0, 0, 0, 0) == pytest.approx(-1)
 def test_1():
-    assert calc_angle(1, 1, 2, 2) == pytest.approx(0)
+    assert comp_angle(1, 1, 2, 2) == pytest.approx(0)
 
 def test_2():
-    assert calc_angle(0, 1, 1, 0) == pytest.approx(math.pi / 2)
+    assert comp_angle(0, 1, 1, 0) == pytest.approx(math.pi / 2)
