89f7c7da62df333799b1e028d8f3cefc88ba71ff
Pius <piuskri@gmail.com>
2022-10-27 15:02:49 +0800

    add matrix class + test

---
+++ hw3/iar1000/_matrix.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <iomanip>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+#include <mkl.h>
+
+namespace py = pybind11;
+
+class Matrix {
+    public:
+
+        Matrix(size_t nrow, size_t ncol) : m_nrow(nrow), m_ncol(ncol) {
+            size_t nelement = nrow * ncol;
+            m_buffer = new double[nelement];
+        }
+
+        ~Matrix() {
+            delete[] m_buffer;
+        }
+
+        double   operator() (size_t row, size_t col) const {
+            return m_buffer[row*m_ncol + col];
+        }
+
+        double & operator() (size_t row, size_t col) {
+            return m_buffer[row*m_ncol + col];
+        }
+
+
+        size_t nrow() const { return m_nrow; }
+        size_t ncol() const { return m_ncol; }
+
+    private:
+
+        size_t m_nrow = 0;
+        size_t m_ncol = 0;
+        double * m_buffer = nullptr;
+
+        friend Matrix multiply_naive(Matrix const &mat1, Matrix const &mat2);
+        friend Matrix multiply_tile(Matrix const &mat1, Matrix const &mat2, size_t const ts);
+        friend Matrix multiply_mkl(Matrix const &mat1, Matrix const &mat2);
+        friend bool operator==(Matrix const &mat1, Matrix const &mat2);
+};
+
+bool operator==(Matrix const &mat1, Matrix const &mat2) {
+    for (size_t i = 0; i < mat1.nrow(); ++i) {
+        for (size_t j = 0; j < mat1.ncol(); ++j){
+            if (mat1(i, j) != mat2(i, j)){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2) {
+    Matrix result(mat1.nrow(), mat2.ncol());
+
+    for (size_t i = 0; i < mat1.nrow(); ++i){
+        for (size_t k = 0; k < mat2.ncol(); ++k){
+            double worker = 0;
+            for (size_t j = 0; j < mat1.ncol(); ++j){
+                worker += mat1(i, j) * mat2(j, k);
+            }
+            result(i, k) = worker;
+        }
+    }
+
+    return result;
+}
+
+Matrix multiply_mkl(Matrix const &mat1, Matrix const &mat2){
+    mkl_set_num_threads(1);
+
+    Matrix result(mat1.nrow(), mat2.ncol());
+    cblas_dgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        mat1.nrow(),
+        mat2.ncol(),
+        mat1.ncol(),
+        1.0,
+        mat1.m_buffer,
+        mat1.ncol(),
+        mat2.m_buffer,
+        mat2.ncol(),
+        0.0,
+        result.m_buffer,
+        result.ncol()
+    );
+
+    return result;
+}
+
+// reference: https://stackoverflow.com/questions/15829223/loop-tiling-blocking-for-large-dense-matrix-multiplication
+Matrix multiply_tile(const Matrix &mat1, const Matrix &mat2, size_t ts){
+    Matrix result(mat1.nrow(), mat2.ncol());
+    for (size_t i0 = 0; i0 < mat1.nrow(); i0 += ts){
+        for (size_t j0 = 0; j0 < mat2.ncol(); j0 += ts){
+            for (size_t k0 = 0; k0 < mat2.ncol(); k0 += ts){
+                for (size_t i = i0; i < std::min(i0 + ts, mat1.nrow()); i++){
+                    for (size_t j = j0; j < std::min(j0 + ts, mat2.ncol()); j++){
+                        for (size_t k = k0; k < std::min(ts + k0, mat1.nrow()); k++){
+                            result(i, j) += mat1(i, k) * mat2(k, j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+PYBIND11_MODULE(_matrix, m) {
+    m.doc() = "for fun with matrices";
+    m.def("multiply_naive", &multiply_naive);
+    m.def("multiply_mkl", &multiply_mkl);
+    m.def("multiply_tile", &multiply_tile);
+
+    py::class_<Matrix>(m, "Matrix")
+        .def(py::init<size_t, size_t>())
+        .def("__setitem__", [](Matrix &self, std::pair<size_t, size_t> index, double val) { self(index.first, index.second) = val; })
+        .def("__getitem__", [](Matrix &self, std::pair<size_t, size_t> index) { return self(index.first, index.second); })
+        .def("__eq__", &operator==)
+        .def_property("nrow", &Matrix::nrow, nullptr)
+        .def_property("ncol", &Matrix::ncol, nullptr);
+    }
+
+
+
---
+++ hw3/iar1000/test_matrix.py
@@ -0,0 +1,109 @@
+import _matrix
+import math
+import numpy as np
+import pytest
+import timeit
+
+
+class TestMatrix:
+    def get_matrix(self, size, factor):
+        mat = _matrix.Matrix(size, size)
+
+        for i in range(size):
+            for j in range(size):
+                mat[i, j] = (i * size + j) * factor
+
+        return mat
+
+    # from validation.py
+    def test_basic(self):
+        size = 100
+        mat1 = self.get_matrix(size, 1)
+        mat2 = self.get_matrix(size, 1)
+        mat3 = self.get_matrix(size, 0)
+
+        assert size == mat1.nrow
+        assert size == mat1.ncol
+        assert size == mat2.nrow
+        assert size == mat2.ncol
+        assert size == mat3.nrow
+        assert size == mat3.ncol
+
+        assert 2 == mat1[0, 1]
+        assert size+2 == mat1[1, 1]
+        assert size*2 == mat1[1, size-1]
+        assert size*size == mat1[size-1, size-1]
+
+        for i in range(mat1.nrow):
+            for j in range(mat1.ncol):
+                assert mat1[i, j] == mat2[i, j]
+                assert 0 == mat3[i, j]
+
+        assert mat1 == mat2
+        assert mat1 is not mat2
+
+
+    def test_zero(self):
+        size = 100
+        mat1 = self.get_matrix(size, 0)
+        mat2 = self.get_matrix(size, 1)
+
+        mat3_naive = _matrix.multiply_naive(mat1, mat2)
+        mat3_mkl = _matrix.multiply_mkl(mat1, mat2)
+
+        for i in range(size):
+            for j in range(size):
+                assert 0 == mat3_naive[i, j]
+                assert 0 == mat3_mkl[i, j]
+
+
+    def check_tile(self, mat1, mat2, ts):
+
+        # baseline
+        result_mkl = _matrix.multiply_mkl(mat1, mat2)
+
+        # tiled
+        if ts < 1:
+            result_tile = _matrix.multiply_naive(mat1, mat2)
+            tile_str = "_matrix.multiply_naive(mat1, mat2)"
+        else:
+            result_tile = _matrix.multiply_tile(mat1, mat2, ts)
+            tile_str = "_matrix.multiply_tile(mat1, mat2, ts)"
+        
+
+        for i in range(result_tile.nrow):
+            for j in range(result_tile.ncol):
+                assert mat1[i,j] != result_mkl[i,j]
+                assert result_tile[i,j] == result_mkl[i,j]
+
+        ns = dict(_matrix=_matrix, mat1=mat1, mat2=mat2, ts=ts)
+        t_tile = timeit.Timer(tile_str, globals=ns)
+        t_mkl = timeit.Timer('_matrix.multiply_mkl(mat1, mat2)', globals=ns)
+
+        time_tile = min(t_tile.repeat(10, 1))
+        time_mkl = min(t_mkl.repeat(10, 1))
+        ratio = time_tile/time_mkl
+
+        return ratio, time_tile
+
+    def test_tile(self):
+        size = 1000
+        mat1 = self.get_matrix(size, 1)
+        mat2 = self.get_matrix(size, 1)
+
+        # todo
+        ratio0, time0 = self.check_tile(mat1, mat2, -1)
+        print("naive ratio:", ratio0)
+        ratio16, time16 = self.check_tile(mat1, mat2, 16)
+        print("tile 16 ratio:", ratio16)
+        print("time16/time0:", time16/time0)
+        assert ratio16/ratio0 <= 0.8
+        ratio17, time17 = self.check_tile(mat1, mat2, 17)
+        print("tile 17 ratio:", ratio17)
+        print("time17/time0:", time17/time0)
+        assert ratio17/ratio0 <= 0.8
+
+        ratio19, time19 = self.check_tile(mat1, mat2, 19)
+        print("tile 19 ratio:", ratio19)
+        print("time19/time0:", time19/time0)
+        assert ratio19/ratio0 <= 0.8
