0186102d092c05cfad603fe4c79cdb624006da73
hychiu02 <hychiu1002@gmail.com>
2022-10-30 01:19:34 +0800

    hychiu02-hw3-attempt4

--- hw3/hychiu02/Matrix.h
+++ hw3/hychiu02/Matrix.h
@@ -20,15 +20,21 @@ public:
         : m_nrow(other.nrow()), m_ncol(other.ncol())
     {
         reset_buffer(other.nrow(), other.ncol());
-        (*this) = other;
+        for (size_t i=0; i<m_nrow; ++i){
+            for (size_t j=0; j<m_ncol; ++j) {
+                (*this)(i,j) = other(i,j);
+            }
+        }
     }
     Matrix(size_t nrow, size_t ncol, std::vector<double> const & vec)
         : m_nrow(nrow), m_ncol(ncol)
     {
-        reset_buffer(other.m_nrow, other.m_ncol);
+        reset_buffer(m_nrow, m_nrow);
+        size_t k = 0;
         for (size_t i=0; i<m_nrow; ++i){
             for (size_t j=0; j<m_ncol; ++j) {
-                (*this)(i,j) = other(i,j);
+                (*this)(i,j) = vec[k];
+                ++k;
             }
         }
     }
